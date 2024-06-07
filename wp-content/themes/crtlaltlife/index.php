f82aa6e5465068b65d5a5ebf16b344b06b21df16
SMcGregor199 <shaynemcgregor1@gmail.com>
2024-06-07 13:53:17 -0400

    add home php file

--- wp-content/themes/crtlaltlife/index.php
+++ wp-content/themes/crtlaltlife/index.php
@@ -1,14 +1,7 @@
 <?php get_header(); ?>
 <?php
 if (is_page('home')) {
-	$args = ["post_type" => "video_games"];
-
-	$loop = new WP_Query($args);
-
-	while ($loop->have_posts()) : $loop->the_post();
-		echo "<p>" . get_the_title() ."</p>"; 
-	endwhile;
-	wp_reset_postdata();
+include("templates/pages/home.php");
 }
 
 if (is_page('list')) {
---
+++ wp-content/themes/crtlaltlife/templates/pages/home.php
@@ -0,0 +1,16 @@
+
+<section>
+	<inner-column>
+<?php 
+
+	$args = ["post_type" => "video_games"];
+
+	$loop = new WP_Query($args);
+
+	while ($loop->have_posts()) : $loop->the_post();
+		echo "<p>" . get_the_title() ."</p>"; 
+	endwhile;
+	wp_reset_postdata();
+?>
+	</inner-column>
+</section>
