b845b5f10e226ebf3ddf50de93bd596dc0ebf973
SMcGregor199 <shaynemcgregor1@gmail.com>
2024-06-08 08:35:18 -0400

    testing relationships between post types

--- wp-content/themes/crtlaltlife/index.php
+++ wp-content/themes/crtlaltlife/index.php
@@ -13,6 +13,19 @@ if (is_page('list')) {
 		include("templates/components/video-game-card.php");
 	endwhile;
 	wp_reset_postdata();
+
+	$args = ["post_type" => "game_franchise"];
+	$loop = new WP_Query($args);
+	while ($loop->have_posts()) : $loop->the_post();
+		foreach(get_field('games_in_series') as $gameInSeries){
+			if(get_the_title($gameInSeries) == "Sonic Forces"){
+				echo "<span>" . get_the_title($gameInSeries) . "</span>";
+				echo the_field('description');
+			}
+		}
+	endwhile;
+	wp_reset_postdata();
+	
 }
 
 if(is_singular('video_games')){
