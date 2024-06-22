45dba9560c4b95028ec8dd866f34ce0119e448cc
SMcGregor199 <shaynemcgregor1@gmail.com>
2024-06-22 13:04:15 -0400

    add image field to video games

--- wp-content/themes/crtlaltlife/templates/components/video-game-card.php
+++ wp-content/themes/crtlaltlife/templates/components/video-game-card.php
@@ -1,4 +1,19 @@
+<?php 
+	$coverArt = get_field('cover_art');
+
+	if(empty($coverArt)){
+		$coverArt = "https://peprojects.dev/images/square.jpg";
+	} else {
+		$coverArt = $coverArt["url"];
+	}
+?>
+
 <div class="video-game-card">
+	<?php if($coverArt) { ?>
+	<picture class="cover-art" style="background-image:url('<?=$coverArt?>') ">
+		<!-- <img src="<?=$coverArt?>" alt="#"> -->
+	</picture>
+	<?php } ?>
 	<h2><?php echo "<p>" . get_the_title() ."</p>"; ?></h2>
 	<h4><?php the_field('developer') ?></h4>
 	<h4><?php the_field('publisher') ?></h4>
